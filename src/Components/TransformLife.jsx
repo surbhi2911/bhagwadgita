ecc197ad43529b03ae207a79b9d1d883bf345e72
surbhi <surbhipansuriya633@gmail.com>
2025-03-16 20:13:44 +0530

    add images

--- src/Components/TransformLife.jsx
+++ src/Components/TransformLife.jsx
@@ -36,31 +36,21 @@ function TransformLife() {
     ]
     return (
         <div className='lightviolet '>
-            <div className=" darkviolet orangecolor title  text-center">
-                Breaking Barriers: A Transformative Talk
-            </div>
-            <div className='container'>
-                <div className="row align-items-center justify-content-center">
-                    <div className='col-lg-5 col-10'>
-                        <p className='text-light lh-lg text-center' style={{ fontFamily: "Hind Vadodara" }}><span className='fw-bold orangecolor'>શ્રીમદ ભગવદ ગીતા </span> માત્ર એક પવિત્ર ગ્રંથ નથી, તે જીવન જીવવાની કળા, જ્ઞાન અને આત્મસાધનાનો અદ્ભુત માર્ગદર્શક છે. મહાભારતના યુદ્ધક્ષેત્રે ભગવાન શ્રીકૃષ્ણ દ્વારા અર્જુનને અપાયેલું આ દિવ્ય ઉપદેશ આજેય માનવજીવન માટે પ્રેરણારૂપ છે. તે આપણને ધર્મ, કર્તવ્ય અને નિષ્કામ કર્મની મહત્તા શીખવે છે. સફળતા ફક્ત ભૌતિક સંપત્તિમાં નથી, પરંતુ નિષ્ઠાપૂર્વક કરેલા કર્મ અને આત્મસુદ્ધિમાં છે. જીવનના દરેક સંજોગોમાં, ગીતા આપણને માર્ગદર્શન અને શાંતિ પ્રદાન કરે છે, જે અંતે આત્મસાક્ષાત્કાર અને દિવ્ય સત્ય તરફ દોરી જાય છે.
-                            આ પવિત્ર શાસ્ત્રના સુવાક્યો અને જીવનપ્રેરક શિક્ષણ પર વિશેષ ઉલ્લેખ કરવા માટે,<span className='fw-bold orangecolor'> શ્રી ગોવિંદ કાકા ધોળકિયા ખાસ વક્તા</span> તરીકે ઉપસ્થિત રહેશે. તેમના જીવન અનુભવ અને આધ્યાત્મિક વિઝનથી આપણે ગીતા ના સાદગીભર્યા પણ ઊંડા મર્મને વધુ સારી રીતે સમજી શકીશું.</p>
-                    </div>
-                    <div className="col-lg-7 col-10">
-                        <div className="row align-items-center justify-content-center">
-                            <div className="col-lg-8 col-md-7 col-sm-6 col-10 text-center">
-                                <img src={require('../Images/Geeta Status PNg.jpg')} alt="" className='img-fluid' />
-                            </div>
-                            <div className="col-lg-4 col-md-5 col-sm-6 col-10 text-center">
-                                <img src={require('../Images/SINGER.jpgs.jpg')} alt="" className='img-fluid' style={{ height: "500px" }} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
             <div className="container">
+                <div className="text-center">
+                    <img src={require('../Images/Geeta Status PNg.jpg')} alt="" className='img-fluid py-3' style={{ width: "350px" }} />
+                </div>
+                <div className='text-center col-sm-8 col-10 mx-auto'>
+                    <p className='text-light lh-lg' style={{ fontFamily: "Hind Vadodara" }}><span className='fw-bold orangecolor'>શ્રીમદ્ ભગવદ્ ગીતા </span> એ આપણા હિન્દુ સનાતન સાહિત્યનો હૃદય સમાન ગ્રંથ છે અને જ્ઞાન,કર્મ અને ભક્તિના અજોડ સંગમથી સફળતાના શિખરો પાર કરી શકાય છે જેનું જીવતું જાગતું ઉદાહરણ એટલે આપણા સૌના વ્હાલા ગોવિંદકાકા એમના જીવનમાં ગીતા દ્વારા થયેલા બદલાવ વિશે માર્ગદર્શન આપશે</p>
+                </div>
                 <div className="text-center py-1">
                     <div className='orangecolor textsizefont '>"First Time In Surat"</div>
+                    <div className="row d-flex justify-content-center py-3 ">
+                        <div className="col-10 col-md-4 h-75">
+                            <img src={require("../Images/SINGER.jpgs.jpg")} alt="" className='object-fit-contain rounded-4 border border-secondary border-4 w-100 h-75' />
+                        </div>
+                    </div>
                     <h1 className="text-white   title">
                         Philosophy With Western Kirtan Deep Wisdom, Soulful Devotion.
                     </h1>
