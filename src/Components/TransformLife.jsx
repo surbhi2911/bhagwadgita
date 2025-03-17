c234f692b957da5d27cfd7aa8aeca949e95d1dab
tankvaishali <vaishalitank28603@gmail.com>
2025-03-17 09:47:01 +0530

    first time surat add

--- src/Components/TransformLife.jsx
+++ src/Components/TransformLife.jsx
@@ -11,7 +11,7 @@ function TransformLife() {
                     <p className='text-light lh-lg' style={{ fontFamily: "Hind Vadodara" }}><span className='fw-bold orangecolor'>શ્રીમદ્ ભગવદ્ ગીતા </span> એ આપણા હિન્દુ સનાતન સાહિત્યનો હૃદય સમાન ગ્રંથ છે અને જ્ઞાન,કર્મ અને ભક્તિના અજોડ સંગમથી સફળતાના શિખરો પાર કરી શકાય છે જેનું જીવતું જાગતું ઉદાહરણ એટલે આપણા સૌના વ્હાલા ગોવિંદકાકા એમના જીવનમાં ગીતા દ્વારા થયેલા બદલાવ વિશે માર્ગદર્શન આપશે</p>
                 </div>
                 <div className="text-center py-1">
-                    {/* <div className='orangecolor textsizefont '>"First Time In Surat"</div> */}
+                    <div className='orangecolor textsizefont '>"First Time In Surat"</div>
                     <div className="row d-flex justify-content-center py-3 ">
                         <div className="col-12 col-md-4 h-75">
                             <img src={require("../Images/SINGER.jpgs.jpg")} alt="" className='object-fit-contain rounded-4 border border-secondary border-4 w-100 h-75' />
